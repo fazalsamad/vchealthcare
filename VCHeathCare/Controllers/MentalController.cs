adc8e564be4da9341d6567a9a424b178debca837
Fazal Samad <fazalsamad555@gmail.com>
2020-11-12 03:40:12 -0800

    mesage

---
+++ VCHeathCare/Controllers/MentalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VCHeathCare.Controllers
+{
+    public class MentalController : Controller
+    {
+        // GET: Mental
+        public ActionResult mental()
+        {
+            return View();
+        }
+    }
+}
